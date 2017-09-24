23ac6f2f8fb87110ce5ad05b4a6bf9a172ac410d
LEFRANCOIS Maxime <maxime.lefrancois.86@gmail.com>
2017-09-24 22:17:47 +0200

    analysis of IFC4 property sets in notes.txt, and added first queries

--- src/main/java/com/github/w3clbdcg/pset/GenerateVocabulary.java
+++ src/main/java/com/github/w3clbdcg/pset/GenerateVocabulary.java
@@ -8,7 +8,7 @@ package com.github.w3clbdcg.pset;
 import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
 import com.github.thesmartenergy.sparql.generate.jena.engine.PlanFactory;
 import com.github.thesmartenergy.sparql.generate.jena.engine.RootPlan;
-import com.github.thesmartenergy.sparql.generate.jena.locator.LocatorFileAccept;
+import com.github.thesmartenergy.sparql.generate.jena.locator.LocatorDirectoryAccept;
 import com.github.thesmartenergy.sparql.generate.jena.query.SPARQLGenerateQuery;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,7 +19,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.system.stream.StreamManager;
@@ -34,6 +33,8 @@ public class GenerateVocabulary {
 
     private static final Logger LOG = LogManager.getLogger(GenerateVocabulary.class);
     
+    private static final String base = "https://w3id.org/product/props/";
+    private static final int MAX = 30;
 
     /**
      * @param args - arguments. Only the first argument is important: it must be absent, IFC4, or IFC2X3
@@ -53,37 +54,41 @@ public class GenerateVocabulary {
 
         // initialize stream manager
         final StreamManager sm = SPARQLGenerate.getStreamManager();
-        sm.addLocator(new LocatorFileAccept(resourcesDir.toURI().getPath()));
+        sm.addLocator(new LocatorDirectoryAccept(base, resourcesDir));
 
-        // load query
+//         load query
         final String queryLocation = "queries/" + focus + "-transform.rqg";
         LOG.info("Applying query " + queryLocation + " on property sets from " + focus);
         final String queryString = IOUtils.toString(sm.open(queryLocation), "UTF-8");
 
-        final String baseLocation = "ontologies/base.ttl";
+        final String baseLocation = "ontologies/" + focus + "-base.ttl";
         final Model model = RDFDataMgr.loadModel(baseLocation,Lang.TTL);
+        
 
         final String outputLocation = focus + "-output.ttl";
         try (FileOutputStream output = new FileOutputStream(new File(outputLocation))) {
 
             // walk in directories that contain string "focus".
             Iterator<File> fileit = FileUtils.iterateFiles(new File(resourcesDir, focus), new String[]{"xml"}, true);
-            while (fileit.hasNext()) {
+
+            SPARQLGenerateQuery query = (SPARQLGenerateQuery) QueryFactory.create(queryString, SPARQLGenerate.SYNTAX);
+
+            RootPlan queryPlan = PlanFactory.create(query);
+            int i=0;
+            while (fileit.hasNext() && i++<MAX) {
                 File psetFile = fileit.next();
 
                 String relativePath = resourcesDir.toURI().relativize(psetFile.toURI()).toString();
-                System.out.println(relativePath);
-                sm.getLocationMapper().addAltEntry("accept:*/*:https://w3id.org/product/pset/" + relativePath, relativePath);
-                String actualQuery = queryString.replace("replace-with-source-url", relativePath);
-                SPARQLGenerateQuery query = (SPARQLGenerateQuery) QueryFactory.create(actualQuery, SPARQLGenerate.SYNTAX);
 
-                RootPlan queryPlan = PlanFactory.create(query);
+                sm.getLocationMapper().addAltEntry(base + "replace-with-source-url", base + relativePath);
+
                 model.add(queryPlan.exec());
-
+                
+                sm.getLocationMapper().removeAltEntry(base + "replace-with-source-url");
             }
             model.write(output, "TTL");
 
-            LOG.info("Transformation successful, see file at " + queryLocation + " on property sets from " + focus);
+            LOG.info("Transformation successful, see file at " + outputLocation + " on property sets from " + focus);
 
         }
 
