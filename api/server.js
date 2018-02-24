98e1121d91edc5eb2a281d1102795fe9f39a5f02
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2018-02-24 15:03:55 +0100

    Changed query

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors()); //Cross Origin Resource Sharing
 
 //Either get port from environment variable or config file
-app.set('port', (process.env.PORT || 3000));  
+app.set('port', (process.env.PORT || 3000));
 
 //Static files location
 app.use(express.static('./dist'));
@@ -22,22 +22,9 @@ app.use(express.static('./dist'));
  */
 app.get('/id/:id', (req, res) => {
 
-    var id = req.params.id;
+    var wikiId = req.params.id;
 
-    var query = `
-    PREFIX props: <https://w3id.org/props#>
-    CONSTRUCT {
-        ?item a props:Property ;
-            rdfs:label ?label ;
-            schema:description ?description .
-    }
-    WHERE {
-        BIND(wd:Q487005 as ?item)
-        ?item rdfs:label ?label ;
-            schema:description ?description .
-    }`;
-
-    const url = wdk.getWikidataIdsFromWikipediaTitles(id);
+    const url = wdk.getWikidataIdsFromWikipediaTitles(wikiId);
 
     res.setHeader('content-type', 'text/turtle');
 
@@ -45,8 +32,25 @@ app.get('/id/:id', (req, res) => {
         try {
             // Get wikidata id
             var data = await rp(url);
+            
             var jsonData = JSON.parse(data);
             var id = Object.keys(jsonData.entities)[0];
+
+            var query = `
+            PREFIX props: <https://w3id.org/props#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            
+            CONSTRUCT {
+                props:${wikiId} a owl:Class ;
+                    rdfs:subClassOf schema:Property ;
+                    rdfs:label ?label ;
+                    schema:description ?description .
+            }
+            WHERE {
+                BIND(wd:${id} as ?item)
+                ?item rdfs:label ?label ;
+                    schema:description ?description .
+            }`;
 
             // Do construct query on resource
             var queryResult = await rp({uri: 'https://query.wikidata.org/sparql', qs: {query: query}, headers: {'Accept': 'text/turtle'}});
